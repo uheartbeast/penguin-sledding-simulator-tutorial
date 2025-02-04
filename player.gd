593d509609ad253955c1bd48dfdfc173dea0152c
Heartbeast <heartbeast.studios@gmail.com>
2025-02-04 16:43:15 -0700

    5

--- player.gd
+++ player.gd
@@ -9,14 +9,21 @@ extends CharacterBody2D
 @export var jump_force: = 180
 @export var unjump_force: = 25
 @export var landing_acceleration: = 2250.0
+@export var air_jump_speed_reduction: = 1500
+@export var coyote_time_amount: = 0.15
 
 var target_tilt: = 0.0
+var air_jump: = true
+var coyote_time: = 0.0
 
 @onready var anchor: Node2D = $Anchor
 @onready var sprite_2d: Sprite2D = $Anchor/Sprite2D
 
 func _physics_process(delta: float) -> void:
-	if is_on_floor():
+	coyote_time += delta
+	
+	if is_on_floor() or coyote_time <= coyote_time_amount:
+		air_jump = true
 		target_tilt = 0
 		if velocity.x <= max_speed:
 			velocity.x = move_toward(velocity.x, max_speed, acceleration * delta)
@@ -30,16 +37,31 @@ func _physics_process(delta: float) -> void:
 		
 		velocity.x = move_toward(velocity.x, 0, air_friction * delta)
 		
+		if Input.is_action_just_pressed("ui_up") and air_jump:
+			velocity.y = -jump_force
+			velocity.x -= air_jump_speed_reduction * delta
+			air_jump = false
+			var tween = create_tween()
+			tween.tween_property(sprite_2d, "rotation_degrees", 0, 0.4).from(360 + sprite_2d.rotation_degrees)
+			tween.play()
+		
 		if Input.is_action_just_released("ui_up"):
 			velocity.y = unjump_force
+		
 		if velocity.y > 0:
 			velocity.y += down_gravity * delta
 		else:
 			velocity.y += up_gravity * delta
 	
 	var previous_velocity = velocity
+	var was_on_floor = is_on_floor()
 	
 	move_and_slide()
+	
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	
+	if just_left_ledge:
+		coyote_time = 0
 	
 	if velocity.y == 0 and previous_velocity.y > 5:
 		anchor.scale = Vector2(1.5, 0.8)
